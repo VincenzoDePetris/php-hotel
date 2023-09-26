12560cbf8f35d8d9cd40ef7294113c9adafd004a
VincenzoDePetris <vince02@live.it>
2023-09-26 16:13:56 +0200

    add: bootstrap and table

--- hotel.php
+++ hotel.php
@@ -40,11 +40,7 @@
 
     ];
 
-    foreach ($hotels as $hotelClass){
-      foreach ($hotelClass as $name) {
-        echo $name, '<br>';
-      }
-    }
+     
 
 ?>
 
@@ -54,8 +50,43 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Hotel</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-  
+  <div class="d-flex justify-content-center mt-5 mx-5 ">
+    <?php
+      echo "<table class='table table-success table-striped'>";
+        echo "<thead>";
+          echo "<tr>";
+            foreach ($hotels as $hotelClass) {
+              echo '<td>',$hotelClass['name'] ,'</td>';
+            }
+          echo "</tr>";
+        echo "</thead>"; 
+        echo "<tbody>"; 
+        echo "<tr>";
+        foreach ($hotels as $hotelClass) {
+          echo '<td>',$hotelClass['description'] ,'</td>';
+        }
+        echo "</tr>";
+        echo "<tr>";
+        foreach ($hotels as $hotelClass) {
+          echo '<td>',$hotelClass['parking'] ,'</td>';
+        }
+        echo "</tr>";
+        echo "<tr>";
+        foreach ($hotels as $hotelClass) {
+          echo '<td>',$hotelClass['vote'] ,'</td>';
+        }
+        echo "</tr>";
+        echo "<tr>";
+        foreach ($hotels as $hotelClass) {
+          echo '<td>',$hotelClass['distance_to_center'] ,'</td>';
+        }
+        echo "</tr>";
+        echo "</tbody>"; 
+      echo "</table>"; 
+    ?>
+  </div>
 </body>
 </html>
